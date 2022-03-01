089f0be728d474d1d2a71092684d2a272c11bde3
daviddlhz <vidda1959@gmail.com>
2022-03-01 00:00:20 -0500

    [FEACTURE] - Transfer friend component v1

--- src/app/modules/dashboard/transfer-friend/index/transfer-friend.component.ts
+++ src/app/modules/dashboard/transfer-friend/index/transfer-friend.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { typeTransaction } from 'src/app/core/domain/enum/typeTransaction.enum';
+import { ITransaction } from 'src/app/core/domain/interfaces/ITransaction';
+import { ITransferRepository } from 'src/app/core/domain/repository/ITransfer.repository';
 
 @Component({
   selector: 'transfer-friend-component',
@@ -7,6 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferFriendComponent {
 
-  constructor() { }
+  transferTo = 0;
+  amount = 0;
+  description = '';
+
+  constructor(@Inject('transferRepository') private transferService: ITransferRepository) { }
+
+  transfer(): void {
+
+    const transaction: ITransaction = {
+      typeTransaction: typeTransaction.TRANSFER,
+      amount: this.amount, 
+      date: new Date, 
+      transferTo: this.transferTo, 
+      description: this.description
+    } 
+    
+    this.transferService.send(transaction);
+  }
 
 }
--- src/app/modules/dashboard/transfer-friend/transfer-friend.module.ts
+++ src/app/modules/dashboard/transfer-friend/transfer-friend.module.ts
@@ -2,11 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { transferFriendRoutes } from './transfer-friend.routing';
 import { TransferFriendComponent } from './index/transfer-friend.component';
+import { FormsModule } from '@angular/forms';
+import { TransferProvider } from 'src/app/core/domain/providers/transfer.provider';
+import { StorageProvider } from 'src/app/core/domain/providers/storage.provider';
+import { DataProvider } from 'src/app/core/domain/providers/data.provider';
 
 
 @NgModule({
+  
   declarations: [TransferFriendComponent],
-  imports: [RouterModule.forChild(transferFriendRoutes)],
+  
+  imports: [
+    FormsModule,
+    RouterModule.forChild(transferFriendRoutes)
+  ],
+  
   exports: [TransferFriendComponent],
+
+  providers: [
+    TransferProvider,
+    StorageProvider,
+    DataProvider
+  ]
+
 })
 export class TransferFriendModule {}
